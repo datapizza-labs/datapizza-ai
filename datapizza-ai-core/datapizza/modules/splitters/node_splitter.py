0a1ad2b0e82349983a382df5a868d07408d7eeb7
Federaffo <federicoraffoni00@gmail.com>
2025-10-20 12:04:05 +0200

    fix: node_embedder does not return empty chunks anymore

--- datapizza-ai-core/datapizza/modules/splitters/node_splitter.py
+++ datapizza-ai-core/datapizza/modules/splitters/node_splitter.py
@@ -21,7 +21,8 @@ class NodeSplitter(Splitter):
     def _node_to_chunks(self, nodes: list[Node]) -> list[Chunk]:
         return [
             Chunk(id=str(node.id), text=node.content, metadata=node.metadata)
-            for node in nodes  #  if node.content
+            for node in nodes
+            if node.content
         ]
 
     def split(self, node: Node) -> list[Chunk]:
